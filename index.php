84cf442b9cac684f1cc5621a8fbe03a10ecb4b9c
cindychi <123279185+cindychi@users.noreply.github.com>
2024-02-29 23:11:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,110 +4,111 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <!-- Bootstrap JS dependencies -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COSI 127b</title>
+    
 </head>
 <body>
+    <h1>IMDB Movie Database</h1>
+
+    <div style="display: flex;">
+    <form action="tabs.php" method="post">
+        <input type="submit" name="v_tables" value="View All Tables">
+        <input type="submit" name="v_actors" value="View All Actors">
+    </form>
+    <form action="allmovies.php" method="post">
+        <input type="submit" name="v_movies" value="View All Movies">
+    </form>
+</div>
+
+   
+
+
+    <!-- Home button -->
+    <a href="index.php" class="btn btn-primary">Home</a>
+
+    <br>
     <div class="container">
-        <h1 style="text-align:center">COSI 127b</h1><br>
-        <h3 style="text-align:center">Connecting Front-End to MySQL DB</h3><br>
-    </div>
-    <div class="container">
-        <form id="ageLimitForm" method="post" action="your_php_script.php">
+        <form id="genreForm" method="post" action="php_script.php"> <!-- Specify php_script.php as the action -->
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Enter minimum age" name="inputAge" id="inputAge">
+                <input type="text" class="form-control" placeholder="Enter a genre" name="inputgenre_name" id="inputgenre_name">
                 <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="submit" name="submitted" id="button-addon2">Query</button>
+                    <button class="btn btn-outline-secondary" type="submit" name="submitted" id="button-addon2">Query This</button>
                 </div>
             </div>
         </form>
     </div>
+
     <div class="container">
-        <h1>Guests</h1>
-        <?php
-        // we want to check if the submit button has been clicked (in our case, it is named Query)
-        if(isset($_POST['submitted']))
-        {
-            // set age limit to whatever input we get
-            // ideally, we should do more validation to check for numbers, etc. 
-           $ageLimit = $_POST["inputAge"]; 
-        }
-        else
-        {
-            // if the button was not clicked, we can simply set age limit to 0 
-            // in this case, we will return everything
-            $ageLimit = 0;
-        }
-
-        // we will now create a table from PHP side 
-        echo "<table class='table table-md table-bordered'>";
-        echo "<thead class='thead-dark' style='text-align: center'>";
-
-        // initialize table headers
-        // YOU WILL NEED TO CHANGE THIS DEPENDING ON TABLE YOU QUERY OR THE COLUMNS YOU RETURN
-         echo "<tr><th class='col-md-2'>Firstname</th><th class='col-md-2'>Lastname</th></tr></thead>";
-
-        // generic table builder. It will automatically build table data rows irrespective of result
-        class TableRows extends RecursiveIteratorIterator {
-            function __construct($it) {
-                parent::__construct($it, self::LEAVES_ONLY);
-            }
-
-            function current() {
-                return "<td style='text-align:center'>" . parent::current(). "</td>";
-            }
-
-            function beginChildren() {
-                echo "<tr>";
-            }
-
-            function endChildren() {
-                echo "</tr>" . "\n";
-            }
-        }
-
-        // SQL CONNECTIONS
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "COSI127b";
-
-        try {
-            // We will use PDO to connect to MySQL DB. This part need not be 
-            // replicated if we are having multiple queries. 
-            // initialize connection and set attributes for errors/exceptions
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // prepare statement for executions. This part needs to change for every query
-            $stmt = $conn->prepare("SELECT first_name, last_name FROM guests where age>=$ageLimit");
-
-            // execute statement
-            $stmt->execute();
-
-            // set the resulting array to associative. 
-            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-            // for each row that we fetched, use the iterator to build a table row on front-end
-            foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-                echo $v;
-            }
-        }
-        catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-        echo "</table>";
-        // destroy our connection
-        $conn = null;
-    
-    ?>
-
+        <h2>Like a Movie</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <div class="form-group">
+                <label for="motionPictureID">Motion Picture ID:</label>
+                <input type="text" class="form-control" id="motionPictureID" name="motionPictureID">
+            </div>
+            <div class="form-group">
+                <label for="userEmail">Your Email:</label>
+                <input type="email" class="form-control" id="userEmail" name="userEmail">
+            </div>
+            <button type="submit" class="btn btn-primary" name="like">Like</button>
+        </form>
     </div>
+
+
+
+
+    <?php
+// Check if the form has been submitted
+if(isset($_POST['like'])) {
+    // Retrieve form data
+    $motionPictureID = $_POST['motionPictureID'];
+    $userEmail = $_POST['userEmail'];
+
+    // Database connection details
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cosi127_pa1.2";
+
+    try {
+        // Create connection
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // Set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Prepare SQL statement to insert like data into the Likes table
+        $stmt = $conn->prepare("INSERT INTO Likes (mpid, uemail) VALUES (:mpid, :uemail)");
+        // Bind parameters
+        $stmt->bindParam(':mpid', $motionPictureID);
+        $stmt->bindParam(':uemail', $userEmail);
+        // Execute the query
+        $stmt->execute();
+
+        echo "Liked movie successfully!";
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    // Close the database connection
+    $conn = null;
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
